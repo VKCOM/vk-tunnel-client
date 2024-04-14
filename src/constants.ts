077479b3645a49dc8c520c0e1d78490bd6c3af8b
N <pasha-nikitin-2022@mail.ru>
2024-04-14 20:47:33 +0300

    Fix parameters parsing

--- src/constants.ts
+++ src/constants.ts
@@ -1,19 +1,20 @@
-import { HttpProtocol, UserProxyAppSettings, WsProtocol } from './types';
+import { HttpProtocol, UserProxyAppSettingsArgs, WsProtocol } from './types';
 
 export const USER_STORAGE_NAME = '@vkontakte/vk-tunnel';
 
 export const USER_SETTINGS_FILE_NAME = 'vk-tunnel-config.json';
 
 export const DEFAULT_USER_PROXY_APP_SETTINGS = {
-  httpProtocol: process.env.PROXY_HTTP_PROTO ?? HttpProtocol.HTTP,
-  wsProtocol: process.env.PROXY_WS_PROTO ?? WsProtocol.WS,
-  timeout: Number(process.env.PROXY_TIMEOUT ?? 5000),
-  port: Number(process.env.PROXY_PORT ?? 10888),
-  host: process.env.PROXY_HOST ?? 'localhost',
-  app_id: undefined,
-  staging: undefined,
-  endpoints: undefined,
-} as UserProxyAppSettings;
+  'http-protocol': process.env.PROXY_HTTP_PROTO ?? HttpProtocol.HTTP,
+  'ws-protocol': process.env.PROXY_WS_PROTO ?? WsProtocol.WS,
+  'timeout': Number(process.env.PROXY_TIMEOUT ?? 5000),
+  'port': Number(process.env.PROXY_PORT ?? 10888),
+  'host': process.env.PROXY_HOST ?? 'localhost',
+  'insecure': 0,
+  'app_id': undefined,
+  'staging': undefined,
+  'endpoints': undefined,
+} as UserProxyAppSettingsArgs;
 
 export const OAUTH_HOST = 'https://oauth.vk.com/';
 export const API_HOST = 'https://api.vk.com/method/';
